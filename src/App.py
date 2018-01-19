b159cf293c2ca1f8b07846c9309dcd8121e87710
Kevin Joyner <joyner.kevin@gmail.com>
2018-01-19 20:45:52 +0000

    add query and calculate

--- src/App.py
+++ src/App.py
@@ -4,6 +4,8 @@ import datetime
 from CoinbasePrices import EurSellPrices
 from BinancePrices import EthBidPrices
 from MongoLog import LogPrices
+from Query import MongoQueries
+from Calculate import Calculators
 
 NOW = datetime.datetime.now()
 DATEHOUR = datetime.datetime(NOW.year, NOW.month, NOW.day, NOW.hour, 0, 0, 0)
@@ -16,3 +18,11 @@ eth_binance_symbols = eth_bid_prices.fetch_eth(EthEurSellPrice)
 
 log_prices = LogPrices()
 log_prices.eur_bid_prices(eth_binance_symbols)
+
+mongo_queries = MongoQueries()
+prices_df = mongo_queries.hourly_prices_per_symbol_df()
+
+calculators = Calculators()
+statsreport = calculators.stats_report(DATEHOUR, prices_df)
+
+print(statsreport)
