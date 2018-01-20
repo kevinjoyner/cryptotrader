a4d22ffe289d6d9331f55f9892e366a9311c9d83
Kevin Joyner <joyner.kevin@gmail.com>
2018-01-20 07:03:32 +0000

    fix

--- src/Calculate.py
+++ src/Calculate.py
@@ -23,8 +23,8 @@ class Calculators:
             value['recent_std_dev'] = np.std(prices_df[symbol][nineteen_hours_ago:hour_ago].apply(float))
 
             value['seven_day_delta'] = \
-            (prices_df[symbol][hour_ago].apply(float) - prices_df[symbol][seven_days_ago].apply(float)) \
-            / prices_df[symbol][seven_days_ago].apply(float)
+            (float(prices_df[symbol][hour_ago]) - float(prices_df[symbol][seven_days_ago])) \
+            / float(prices_df[symbol][seven_days_ago])
 
             values.append(value)
 
